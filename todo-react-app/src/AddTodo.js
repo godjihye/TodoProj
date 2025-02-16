8e2f3a1ce75d6b5b980afbfa2868923d9a9094b3
godjihye <njih112@gmail.com>
2025-02-16 21:16:25 +0900

    3-2 프론트엔드 서비스 개발

---
+++ todo-react-app/src/AddTodo.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Button, Grid, TextField } from "@mui/material";
+
+const AddTodo = (props) => {
+  // 사용자의 입력을 저장할 임시 오브젝트
+  const [item, setItem] = useState({ title: "" });
+
+  // onInputChange함수 작성
+  const onInputChange = (e) => {
+    setItem({ title: e.target.value });
+    console.log(item);
+  };
+
+  const addItem = props.addItem;
+  const onButtonClick = () => {
+    addItem(item);
+    setItem({ title: "" });
+  };
+
+  const enterKeyEventHandler = (e) => {
+    if (e.key == "Enter") {
+      onButtonClick();
+    }
+  };
+  return (
+    <Grid container style={{ marginTop: 20 }}>
+      <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+        <TextField
+          placeholder="Add Todo here"
+          fullWidth
+          onChange={onInputChange}
+          onKeyUp={enterKeyEventHandler}
+          value={item.value}
+        />
+      </Grid>
+      <Grid xs={1} md={1} item>
+        <Button
+          fullWidth
+          style={{ height: "100%" }}
+          color="secondary"
+          variant="outlined"
+          onClick={onButtonClick}
+        >
+          +
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+export default AddTodo;
---
+++ todo-react-app/src/Todo.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import {
+  ListItem,
+  ListItemText,
+  InputBase,
+  Checkbox,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@mui/material";
+import { DeleteOutline } from "@mui/icons-material";
+
+const Todo = (props) => {
+  const [item, setItem] = useState(props.item);
+  // ReadOnly
+  const [readOnly, setReadOnly] = useState(true);
+  const turnOffReadOnly = () => {
+    setReadOnly(false);
+  };
+  const turnOnReadOnly = (e) => {
+    if (e.key == "Enter") {
+      setReadOnly(true);
+    }
+  };
+  // deleteItem()함수 연결
+  const deleteItem = props.deleteItem;
+  // deleteEventHandler()
+  const deleteEventHandler = () => {
+    deleteItem(item);
+  };
+  // editItem() 연경
+  const editItem = props.editItem;
+  const editEventHandler = (e) => {
+    item.title = e.target.value;
+    editItem();
+  };
+  // checkbox Event handler
+  const checkboxEventHandler = (e) => {
+    item.done = e.target.checked;
+    editItem();
+  };
+  return (
+    <ListItem>
+      <Checkbox checked={item.done} onChange={checkboxEventHandler} />
+      <ListItemText>
+        <InputBase
+          inputProps={{ "aria-label": "naked", readOnly: readOnly }}
+          onClick={turnOffReadOnly}
+          onKeyDown={turnOnReadOnly}
+          onChange={editEventHandler}
+          type="text"
+          id={item.id}
+          name={item.id}
+          value={item.title}
+          multiline={true}
+          fullWidth={true}
+        />
+      </ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Delete Todo" onClick={deleteEventHandler}>
+          <DeleteOutline />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+};
+
+export default Todo;
